50a47670bc8239d99bf11a10485df4eb9461c2f0
Настя <@USER1>
2023-02-19 18:49:33 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1lab/ConsoleApplication1lab.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+struct person
+{
+    int age;
+    char name[20];
+};
+
+struct phone
+{
+    int number;
+    int size;
+
+};
+
+int main()
+{
+    struct person people[] = { {23, "Tom"}, {32, "Bob"}, {26, "Alice"}, {41, "Sam"} };
+    int n = sizeof(people) / sizeof(people[0]);
+    for (int i = 0; i < n; i++)
+    {
+        printf("Name:%s \t Age: %d \n", people[i].name, people[i].age);
+    }
+    return 0;
+}
