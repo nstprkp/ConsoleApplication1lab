a1d2baf15a80631f6deabf831a5b8c9f8ca2403b
nstprkp <113131930+nstprkp@users.noreply.github.com>
2023-02-20 12:20:05 +0300

    Add files via upload

---
+++ lab1sem2mark7-8/lab1sem2mark7-8/lab1sem2mark7-8.c
@@ -0,0 +1,22 @@
+﻿#include "Header.h"
+
+typedef enum Gender
+{
+    MALE,
+    FEMALE
+};
+
+struct product
+{
+    char name[100];
+    int price;
+    int kol;
+    enum Gender gend;
+};
+
+int main()
+{
+    menu();
+    work_prog();
+    return 0;
+}
